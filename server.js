87cbec99341a04766c4bc360fcf0b2f01c40fe81
shamimranapro85 <shamimranaprofessional@gmail.com>
2024-10-18 06:38:16 +0600

    add backend

--- server.js
+++ server.js
@@ -1,8 +1,38 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
-app.use("/", (req, res) => {
-  res.json({name: "shamim"});
+const register_otp_scema = new mongoose.Schema({
+  otp: {
+    type: String,
+    required: true,
+  },
+  payload: {
+    type: Object,
+    required: true,
+  },
+  expireAt: {
+    type: Date,
+    default: () => new Date(Date.now() + 60000),
+  },
+});
+const register_otp_model = mongoose.model(
+  "register_otp_model",
+  register_otp_scema
+);
+
+app.use("/", async (req, res) => {
+  await mongoose.connect("mongodb://localhost:27017/shamimsherpur");
+  console.log("databaseconnect");
+  
+
+  let responsed = await register_otp_model.findOne({
+    email: "shamimranaprofesstional@gmail.com",
+  });
+
+  console.log(responsed);
+
+  res.json({ name: "shamim" });
 });
 
 app.listen(742, () => {
